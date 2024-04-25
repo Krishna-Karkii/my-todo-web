4da0947b608c77581cc237c5ed66024314f8ac8b
krishna <krishnakarki1996@gmail.com>
2024-04-26 01:37:09 +0545

    initial commit

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
-def return_todos(filename="python-todo/todos.txt"):
+def return_todos(filename="todos.txt"):
     with open(filename, "r") as local_Users_file:
         local_Users_list = local_Users_file.readlines()
         return local_Users_list
 
 
-def write_todos(local_users_list, filename="python-todo/todos.txt"):
+def write_todos(local_users_list, filename="todos.txt"):
     with open(filename, "w") as local_Users_file:
         new_Users_list = local_Users_file.writelines(local_users_list)
         return new_Users_list
